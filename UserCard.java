31e2053b0c7102c5740b2334b451ef75ef4a03b9
Methat-i <ice.methat@gmail.com>
2025-03-03 06:35:12 +0700

    P2

---
+++ UserCard.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+// Abstract Class สำหรับเก็บข้อมูลบัตรผู้ใช้
+public abstract class UserCard {
+    protected String username;
+    protected String cardNumber;
+    protected String password;
+    protected int hours;  // เปลี่ยนจากวันเป็นชั่วโมง
+    protected int roomNumber;
+    protected String accessLevel;
+
+    public UserCard(String username, int hours, int roomNumber, String accessLevel) {
+        this.username = username;
+        this.cardNumber = generateCardNumber();
+        this.password = this.cardNumber; // ใช้หมายเลขบัตรเป็นรหัสผ่าน
+        this.hours = hours;
+        this.roomNumber = roomNumber;
+        this.accessLevel = accessLevel;
+    }
+
+    private String generateCardNumber() {
+        Random rand = new Random();
+        return String.format("%04d", rand.nextInt(10000));
+    }
+
+    public abstract void accessRoom();
+
+    public int calculatePrice() {
+        switch (accessLevel) {
+            case "Normal": return hours * 5;
+            case "Medium": return hours * 10;
+            case "High": return hours * 15;
+            default: return 0;
+        }
+    }
+}
